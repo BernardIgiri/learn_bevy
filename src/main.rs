076a78b9da25ef05045b2a4888fb610e41e7d67e
Bernard Igiri <bernard.igiri@gmail.com>
2023-06-30 20:23:47 -0400

    Updated the tutorial code.

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,11 @@
+mod plugins;
+
 use bevy::prelude::*;
-
-struct Person;
-
-struct Name(String);
-
-struct GreetTimer(Timer);
-
-fn add_people(mut commands: Commands) {
-    commands
-        .spawn((Person, Name("Elaina Proctor".to_string())))
-        .spawn((Person, Name("Renzo Hume".to_string())))
-        .spawn((Person, Name("Zayna Nieves".to_string())));
-}
-
-fn greet_people(
-    time: Res<Time>, mut timer: ResMut<GreetTimer>, mut query: Query<(&Person, &Name)>) {
-    timer.0.tick(time.delta_seconds);
-    if timer.0.finished {
-        for (_person, name) in &mut query.iter() {
-            println!("hello {}!", name.0);
-        }
-    }
-}
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut AppBuilder) {
-        app.add_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people.system())
-            .add_system(greet_people.system());
-    }
-}
+use plugins::PeoplePlugin;
 
 fn main() {
-    App::build()
-        .add_default_plugins()
-        .add_plugin(HelloPlugin)
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(PeoplePlugin)
         .run();
 }
---
+++ src/plugins/mod.rs
@@ -0,0 +1,34 @@
+use bevy::prelude::*;
+
+pub struct PeoplePlugin;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".into())));
+    commands.spawn((Person, Name("Renzo Hume".into())));
+    commands.spawn((Person, Name("Zanya Nieves".into())));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
+impl Plugin for PeoplePlugin {
+    fn build(&self, _app: &mut App) {
+        _app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_startup_system(add_people)
+            .add_system(greet_people);
+    }
+}
